6981e6282817c546fbdfbada2626f4bb7ae0fd93
Kobiss <ankobiss@gmail.com>
2017-04-17 15:23:11 +0300

    first commit

--- Task2_ComplementarySequences/src/com/softgroup/task2/Main.java
+++ Task2_ComplementarySequences/src/com/softgroup/task2/Main.java
@@ -1,10 +1,38 @@
 package com.softgroup.task2;
 
+import java.util.*;
+
 /**
  * Created by Andrew on 14.04.2017.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hi");
+
+    public Map<Integer, Integer> getKComplementaryPairs(int sum, int[] arrayOfIntegers) {
+
+        if (arrayOfIntegers == null || arrayOfIntegers.length == 0) {
+            return null;
+        }
+
+        Arrays.parallelSort(arrayOfIntegers);
+
+        Map<Integer, Integer> pairs = new LinkedHashMap<>();
+
+        int firstPointer = 0;
+        int lastPointer = arrayOfIntegers.length-1;
+
+        while (firstPointer < lastPointer) {
+            int currentSum = arrayOfIntegers[firstPointer] + arrayOfIntegers[lastPointer];
+            if (currentSum == sum) {
+                pairs.put(arrayOfIntegers[firstPointer], arrayOfIntegers[lastPointer]);
+                firstPointer++;
+                lastPointer--;
+            } else if (currentSum > sum) {
+                lastPointer--;
+            } else {
+                firstPointer++;
+            }
+        }
+        return pairs;
     }
+
 }
--- Task2_ComplementarySequences/test/com/softgroup/task2/MainTest.java
+++ Task2_ComplementarySequences/test/com/softgroup/task2/MainTest.java
@@ -1,15 +1,63 @@
 package com.softgroup.task2;
 
+import javafx.util.Pair;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import sun.rmi.runtime.Log;
+import sun.swing.BakedArrayList;
+
+import java.util.*;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Andrew on 14.04.2017.
  */
+@RunWith(Parameterized.class)
 public class MainTest {
+
+
+    private Main main;
+
+    @Before
+    public void init(){
+        main = new Main();
+    }
+
+    @Parameterized.Parameter
+    public int sum;
+
+    @Parameterized.Parameter(1)
+    public int[] array;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        List<Object[]> params = new ArrayList<>();
+        params.add(new Object[]{/*sum:*/ 1950, createArray(10000, 10000)});
+        params.add(new Object[]{/*sum:*/ 200, createArray(1000, 1000)});
+        params.add(new Object[]{/*sum:*/ 0, createArray(1000, 1000)});
+        params.add(new Object[]{/*sum:*/ 2000, createArray(1000, 1000)});
+        return params;
+    }
+
+    private static int[] createArray(int arrayLength, int maxElement){
+        Random random = new Random();
+        int[] a = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            a[i] = random.nextInt(maxElement);
+        }
+        return a;
+    }
+
     @Test
-    public void main() throws Exception {
+    public void kompTest() throws Exception{
+        Map<Integer, Integer> map = main.getKComplementaryPairs(sum, array);
+        System.out.println(Arrays.toString(new Map[]{map}));
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            assertEquals(sum, entry.getKey()+entry.getValue());
+        }
     }
 
 }
